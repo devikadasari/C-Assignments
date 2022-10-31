1bf329b29fce101456f7b31fa5666329d8380a45
devikadasari <devikadasari8@gmail.com>
2022-10-31 22:18:18 +0530

    done

---
+++ Assignment6/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+namespace MulticastDelegateDemo
+{
+    public class Anyone
+    {
+        public void GetArea(double Width, double Height)
+        {
+            Console.WriteLine($"Area is {Width * Height}");
+        }
+        public void GetPerimeter(double Width, double Height)
+        {
+            Console.WriteLine($"Perimeter is {2 * (Width + Height)}");
+        }
+        static void Main(string[] args)
+        {
+            Anyone rect = new Anyone();
+            rect.GetArea(23.45, 67.89);
+            rect.GetPerimeter(23.45, 67.89);
+            Console.ReadKey();
+        }
+    }
+}
