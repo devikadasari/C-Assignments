f03acd1e0d32236a33ccd99877e440a7cc695dfa
devikadasari <devikadasari8@gmail.com>
2022-11-01 11:08:06 +0530

    done

--- Assignment6/ConsoleApp1/Program.cs
+++ Assignment6/ConsoleApp1/Program.cs
@@ -1,23 +1,57 @@
-﻿
-using System;
-namespace MulticastDelegateDemo
+﻿using System;
+class program
 {
-    public class Anyone
+    public static void Main()
     {
-        public void GetArea(double Width, double Height)
+        int amount = 2034, deposit, withdraw;
+        int choice, pin = 1234, x = 0;
+        Console.WriteLine("Enter Your 4 Digit Pin ");
+        pin = int.Parse(Console.ReadLine());
+        while (true)
         {
-            Console.WriteLine($"Area is {Width * Height}");
+            Console.WriteLine("WELCOME TO YES BANK ATM SERVICE\n");
+            Console.WriteLine("1. Current Balance\n");
+            Console.WriteLine("2. Withdraw \n");
+            Console.WriteLine("3. Deposit \n");
+            Console.WriteLine("4. Cancel \n");
+            Console.WriteLine("***************\n\n");
+            Console.WriteLine("ENTER YOUR CHOICE : ");
+            choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("\n YOUR CURRENT BALANCE IS Rs : {0} ", amount);
+                    break;
+                case 2:
+                    Console.WriteLine("\n ENTER THE WITHDRAW AMOUNT : ");
+                    withdraw = int.Parse(Console.ReadLine());
+                    if (withdraw % 100 != 0)
+                    {
+                        Console.WriteLine("\n PLEASE ENTER THE AMOUNT IN ABOVE 100");
+                    }
+                    else if (withdraw > (amount - 1000))
+                    {
+                        Console.WriteLine("\n SORRY! INSUFFICENT BALANCE");
+                    }
+                    else
+                    {
+                        amount = amount - withdraw;
+                        Console.WriteLine("\n\n PLEASE COLLECT YOUR CASH");
+                        Console.WriteLine("\n CURRENT BALANCE IS Rs : {0}", amount);
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("\n ENTER THE DEPOSIT AMOUNT");
+                    deposit = int.Parse(Console.ReadLine());
+                    amount = amount + deposit;
+                    Console.WriteLine("YOUR AMOUNT HAS BEEN DEPOSITED SUCCESSFULLY");
+                    Console.WriteLine("YOUR TOTAL BALANCE IS Rs : {0}", amount);
+                    break;
+                case 4:
+                    Console.WriteLine("\n THANK YOU…");
+                    break;
+            }
         }
-        public void GetPerimeter(double Width, double Height)
-        {
-            Console.WriteLine($"Perimeter is {2 * (Width + Height)}");
-        }
-        static void Main(string[] args)
-        {
-            Anyone rect = new Anyone();
-            rect.GetArea(23.45, 67.89);
-            rect.GetPerimeter(23.45, 67.89);
-            Console.ReadKey();
-        }
+        Console.WriteLine("\n\n THANKS FOR USING YES ATM SERVICE");
     }
 }
